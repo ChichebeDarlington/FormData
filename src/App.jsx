a6d351300f6fefce311732a55372d9be6656412c
Chichebe <chichebewebdev@gmail.com>
2023-05-19 04:19:12 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import "./App.css";
+
+function App() {
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    // console.log(formData);
+    // const email = formData.get("email");
+    // const name = formData.get("name");
+    // const password = formData.get("password");
+    // console.log(email, name, password);
+    const data = Object.fromEntries(formData);
+    const values = [...formData.values()];
+    console.log(values);
+    const isEmpty = values.includes("");
+    if (isEmpty) {
+      console.log("Please fill in all data");
+    }
+    // clear inputs
+    // event.currentTarget.reset();
+  };
+
+  const form = () => {
+    return (
+      <form onSubmit={onSubmit}>
+        {/* the input must have the name attribute which must match */}
+        <h1>FORMDATA</h1>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="text" name="password" id="password" />
+        </div>
+        <div className="btn">
+          <button>Submit</button>
+        </div>
+      </form>
+    );
+  };
+  return <div>{form()}</div>;
+}
+
+export default App;
